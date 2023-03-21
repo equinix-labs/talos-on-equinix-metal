f6403d8f4087d9ec0c9bbf7f98babf282aaea07b
Krystian Marek <krystian.marek@eu.equinix.com>
2023-03-21 11:57:41 +0100

    Template update

--- generate_cluster.sh
+++ generate_cluster.sh
@@ -7,6 +7,7 @@ cd "${TOEM_SECRETS_DIR}"
 yq -s '"spec_" + .kind' "${TOEM_PROJECT_ROOT}/${TOEM_MANIFEST_DIR}/${CLUSTER_NAME}.yaml"
 yq e '. | .spec.controlPlaneConfig.controlplane.configPatches[0].value.contents |= load_str("cpem/cpem.yaml")' -i spec_TalosControlPlane.yml
 yq eval-all spec_* > "${CLUSTER_NAME}.yaml"
+rm spec_*
 cd "${TOEM_PROJECT_ROOT}"
 
 echo "k8s_yaml('${TOEM_SECRETS_DIR}/${CLUSTER_NAME}.yaml')" > Tiltfile
