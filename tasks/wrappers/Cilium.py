09b8565501503dd7425cad31d5c2dc5129f83956
Krystian Marek <krystian.marek@eu.equinix.com>
2023-07-31 14:36:46 +0200

    clustermesh

--- tasks/wrappers/Cilium.py
+++ tasks/wrappers/Cilium.py
@@ -1,8 +1,10 @@
 import base64
+import logging
 from typing import Union
 
 import yaml
 from invoke import Context
+from pydantic_yaml import YamlModel
 
 from tasks.controllers.MetalCtrl import MetalCtrl
 from tasks.dao.ProjectPaths import ProjectPaths
@@ -37,19 +39,27 @@ class Cilium:
                 namespace
             ), echo=self._echo)
 
-    def cluster_mesh_connect(self, namespace: Namespace = Namespace.network_services):
-        connected_targets = list()
+    def _get_mesh_client_certs(self, namespace: Namespace = Namespace.network_services):
+        client_certificates = dict()
         for cluster in self._state.constellation:
-            for cluster_b in self._state.constellation:
-                if cluster.name != cluster_b.name and Union[cluster.name, cluster_b.name] not in connected_targets:
-                    # print("Connect {} => {}".format(cluster.name, cluster_b.name))
-                    self._ctx.run("cilium --namespace {} --context {} clustermesh connect --destination-context {}".format(
-                        namespace.value,
-                        'admin@' + cluster.name,
-                        'admin@' + cluster_b.name
-                    ), echo=self._echo)
-                    connected_targets.append(Union[cluster.name, cluster_b.name])
+            client_cert_yaml = self._ctx.run(
+                "kubectl --namespace {} get secret clustermesh-apiserver-client-cert -o yaml".format(
+                    namespace
+                ), hide="stdout", echo=self._echo).stdout
+            client_cert = dict(yaml.safe_load(client_cert_yaml))
 
+            client_certificates[cluster.name] = dict()
+            client_certificates[cluster.name]["{}.etcd-client-ca.crt".format(cluster.name)] = client_cert['data']['ca.crt']
+            client_certificates[cluster.name]["{}.etcd-client.crt".format(cluster.name)] = client_cert['data']['tls.crt']
+            client_certificates[cluster.name]["{}.etcd-client.key".format(cluster.name)] = client_cert['data']['tls.key']
+
+        return client_certificates
+
+    def _fix_cilium_clustermesh_secret(self, cluster_mesh_certs: dict,
+                                       namespace: Namespace = Namespace.network_services):
+        """
+        ...
+        """
         for cluster in self._state.constellation:
             cluster_mesh_config_yaml = self._ctx.run(
                 "kubectl --context admin@{} -n {} get secret cilium-clustermesh -o yaml".format(
@@ -63,23 +73,31 @@ class Cilium:
             del (cluster_mesh_config['metadata']['uid'])
 
             cluster_mesh_config_data_keys = cluster_mesh_config['data'].keys()
+            print("#" * 5 + " " + cluster.name + " =>")
             for key in cluster_mesh_config_data_keys:
-                if key in self._state.constellation:
+                if key in self._state.constellation:  # Patch config
                     decoded_yaml = base64.b64decode(cluster_mesh_config['data'][key])
                     endpoint_config = dict(yaml.safe_load(decoded_yaml))
                     # print(cluster.name)
                     # print(endpoint_config)
-                    # metal_ctrl = MetalCtrl(self._state, self._echo, Cluster(name=key))
+                    metal_ctrl = MetalCtrl(self._state, self._echo, Cluster(name=key))
                     endpoint_config['endpoints'] = [
                         "https://{}:2379".format(
-                            get_fqdn(['mesh', key], self._state.secrets, cluster))
-                        # "https://{}:2379".format(
-                        #     metal_ctrl.get_vips(VipRole.mesh).public_ipv4[0]),
+                            get_fqdn(['mesh', key], self._state.secrets, cluster)),
+                        "https://{}:2379".format(
+                            metal_ctrl.get_vips(VipRole.mesh).public_ipv4[0])
                         # "https://clustermesh-apiserver.{}.svc:2379".format(namespace)
                     ]
 
                     encoded_yaml = base64.b64encode(bytes(yaml.safe_dump(endpoint_config), 'utf-8')).decode('utf-8')
                     cluster_mesh_config['data'][key] = encoded_yaml
+                    print("Patched config: {}".format(key))
+                else:  # Patch certificates
+                    for cluster_name in cluster_mesh_certs:
+                        for cert_name in cluster_mesh_certs[cluster_name]:
+                            if key == cert_name:
+                                cluster_mesh_config['data'][key] = cluster_mesh_certs[cluster_name][cert_name]
+                                print("Patched cert: {}".format(key))
 
             paths = ProjectPaths(constellation_name=self._state.constellation.name, cluster_name=cluster.name)
             with open(paths.patch_mesh_secret_file(), 'w') as cluster_mesh_secret_file:
@@ -96,6 +114,25 @@ class Cilium:
             ), echo=self._echo)
 
             self.restart(namespace)
+
+    def _cluster_mesh_connect(self, namespace: Namespace = Namespace.network_services):
+        connected_targets = list()
+        for cluster in self._state.constellation:
+            for cluster_b in self._state.constellation:
+                if cluster.name != cluster_b.name and Union[cluster.name, cluster_b.name] not in connected_targets:
+                    # print("Connect {} => {}".format(cluster.name, cluster_b.name))
+                    self._ctx.run(
+                        "cilium --namespace {} --context {} clustermesh connect --destination-context {}".format(
+                            namespace.value,
+                            'admin@' + cluster.name,
+                            'admin@' + cluster_b.name
+                        ), echo=self._echo)
+                    connected_targets.append(Union[cluster.name, cluster_b.name])
+
+    def cluster_mesh_connect(self, namespace: Namespace = Namespace.network_services):
+        self._cluster_mesh_connect(namespace)
+        # client_mesh_certs = self._get_mesh_client_certs(namespace)
+        # self._fix_cilium_clustermesh_secret(client_mesh_certs, namespace)
 
     def restart(self, namespace: Namespace = Namespace.network_services):
         for cluster in self._state.constellation:
